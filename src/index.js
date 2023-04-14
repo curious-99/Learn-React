09fe213777418b7792caed84ed8199e36d1a7b27
curious-99 <mohit39.ugcs20@iiitranchi.com>
2023-04-14 11:45:43 +0530

    Inline css in  JSX attribute

--- src/index.js
+++ src/index.js
@@ -108,14 +108,25 @@ const img2 = "https://picsum.photos/250/300";
 const img3 = "https://picsum.photos/300/300";
 const img4 = "https://picsum.photos/350/300";
 const link = "http://youtube.com";
+
+const heading = {
+    color:"#fa9191",
+    textAlign:"center",
+    textTransform:'capitalize',
+    fontWeight:'bold',
+    textShadow:'0px 2px 8px #aba696',
+    margin:'50px 0px',
+    fontFamily:'"Varela Round", sans - serif',
+};
+
 ReactDOM.render(
     <>
-        <h1 className='heading'>{`My name is ${name}`}</h1>
+        <h1 style={heading}>{`My name is ${name}`}</h1>
         <div className='img_div'>
-        <img src = {img1} alt ="randomImages"/>
-        <img src = {img2} alt ="randomImages"/>
-        <img src = {img3} alt ="randomImages"/>
-        <a href = {link} target ="_blank"> <img src = {img4} alt ="randomImages"/> </a>
+            <img src={img1} alt="randomImages" />
+            <img src={img2} alt="randomImages" />
+            <img src={img3} alt="randomImages" />
+            <a href={link} target="_blank"> <img src={img4} alt="randomImages" /> </a>
         </div>
     </>,
     document.getElementById('root')
